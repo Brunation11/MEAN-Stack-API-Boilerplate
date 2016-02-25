28b5786b4c7442ad62be6e63e2544437067973c0
Bruno Zatta <bzatta@gmail.com>
2016-02-25 13:15:11 -0500

    create api directory, add api file as router entry point, add routers for users, posts, and categories

---
+++ server/api/category/categoryRoutes.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var logger = require('../../util/logger');
+
+router.route('/')
+  .get(function(req, res) {
+
+  })
+  .post(function(req, res) {
+
+  });
+
+router.route('/:id')
+  .get(function(req, res) {
+
+  })
+  .put(function(req, res) {
+
+  })
+  .delete(function(req, res) {
+
+  });
+
+  module.exports = router;
