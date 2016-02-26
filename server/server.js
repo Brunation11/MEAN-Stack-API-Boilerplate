08e81b0831eb48a211453498a3d8d32eac9f0a86
Bruno Zatta <bzatta@gmail.com>
2016-02-26 14:57:41 -0500

    add db connection to server entry point

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 var apiRouter = require('./api/api');
+var config = require('./config/config');
 var err = require('./middleware/err');
+
+require('mongoose').connect(config.db.url)
 
 require('./middleware/appMiddleware')(app);
 
