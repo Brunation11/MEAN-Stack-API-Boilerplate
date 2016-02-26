2704ffab428b18db4ef448daaa7bb923be318453
Bruno Zatta <bzatta@gmail.com>
2016-02-26 18:04:52 -0500

    add new controller files for category, post, and user with basic crud functions

---
+++ server/api/category/categoryController.js
@@ -0,0 +1,64 @@
+var CategoryModel = require('./categoryModel');
+var _ = require('lodash');
+
+exports.params = function(req, res, next, id) {
+  CategoryModel.findById(id, function(err, category) {
+    if (err) {
+      next(err);
+    } else if (!category) {
+      next(new Error('Category not found'));
+    } else {
+      req.category = category;
+      next();
+    }
+  })
+};
+
+exports.get = function(req, res, next) {
+  CategoryModel.find({}, function(err, categories) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(categories);
+    }
+  })
+};
+
+exports.getOne = function(req, res, next) {
+  var category = req.category;
+  res.json(category);
+};
+
+exports.put = function(req, res, next) {
+  var category = req.category;
+  var update = req.body;
+  _.merge(category, update);
+  category.save(function(err, category) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(category);
+    }
+  })
+};
+
+exports.post = function(req, res, next) {
+  var category = new CategoryModel(req.body);
+  category.save(function(err, category) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(category);
+    }
+  })
+};
+
+exports.delete = function(req, res, next) {
+  req.category.remove(function(err, category) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(category);
+    }
+  })
+};
---
+++ server/api/user/userController.js
@@ -0,0 +1,64 @@
+var UserModel = require('./userModel');
+var _ = require('lodash');
+
+exports.params = function(req, res, next, id) {
+  UserModel.findById(id, function(err, user) {
+    if (err) {
+      next(err);
+    } else if (!user) {
+      next(new Error('User not found'));
+    } else {
+      req.user = user;
+      next();
+    }
+  })
+};
+
+exports.get = function(req, res, next) {
+  UserModel.find({}, function(err, users) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(users);
+    }
+  })
+};
+
+exports.getOne = function(req, res, next) {
+  var user = req.user;
+  res.json(user);
+};
+
+exports.put = function(req, res, next) {
+  var user = req.user;
+  var update = req.body;
+  _.merge(user, update);
+  user.save(function(err, user) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(user);
+    }
+  })
+};
+
+exports.post = function(req, res, next) {
+  var user = new PostModel(req.body);
+  user.save(function(err, user) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(user);
+    }
+  })
+};
+
+exports.delete = function(req, res, next) {
+  req.user.remove(function(err, user) {
+    if (err) {
+      next(err);
+    } else {
+      res.json(user);
+    }
+  })
+};
