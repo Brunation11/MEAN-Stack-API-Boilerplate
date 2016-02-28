6591b9fd1023e5153dbc85ead7d15232d21766ce
Bruno Zatta <bzatta@gmail.com>
2016-02-28 13:56:45 -0500

    update beforeEach hook on category spec

--- server/api/category/spec.js
+++ server/api/category/spec.js
@@ -5,13 +5,21 @@ var CategoryModel = require('./categoryModel');
 require('colors');
 
 describe('[CATEGORY]'.bold.green, function() {
-  var category = {
+  var category;
+  var testData = {
     name: 'test'
   };
 
   beforeEach(function(done) {
     CategoryModel.collection.drop();
-    done();
+    request(app)
+      .post('/api/categories')
+      .send(testData)
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        category = res.body;
+        done();
+      });
   });
 
   describe('#get()'.cyan, function() {
@@ -32,13 +40,13 @@ describe('[CATEGORY]'.bold.green, function() {
     it('should create a new category', function(done) {
       request(app)
         .post('/api/categories')
-        .send(category)
+        .send(testData)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(201)
         .end(function(err, res) {
           expect(res.body).to.be.an('object');
-          expect(res.body).to.have.property('name', category.name);
+          expect(res.body).to.have.property('name', testData.name);
           done();
         });
     });
@@ -47,43 +55,31 @@ describe('[CATEGORY]'.bold.green, function() {
   describe('#get()'.cyan, function() {
     it('should get a specific category', function(done) {
       request(app)
-        .post('/api/categories')
-        .send(category)
+        .get('/api/categories/' + category._id)
         .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
         .end(function(err, res) {
-          var category = res.body;
-          request(app)
-            .get('/api/categories/' + category._id)
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .end(function(err, res) {
-              expect(res.body).to.eql(category);
-              done();
-            });
+          expect(res.body).to.eql(category);
+          done();
         });
     });
   });
 
   describe('#put()'.cyan, function() {
     it('should update a specific category', function(done) {
+      var update  = {
+        name: 'updated test'
+      };
       request(app)
-        .post('/api/categories')
-        .send(category)
+        .put('/api/categories/' + category._id)
+        .send(update)
         .set('Accept', 'application/json')
+        .expect('Category-Type', /json/)
+        .expect(200)
         .end(function(err, res) {
-          var category = res.body;
-          var update  = {
-            name: 'updated test'
-          };
-          request(app)
-            .put('/api/categories/' + category._id)
-            .send(update)
-            .expect('Category-Type', /json/)
-            .expect(200)
-            .end(function(err, res) {
-              expect(res.body).to.have.property('name', update.name);
-              done();
-            });
+          expect(res.body).to.have.property('name', update.name);
+          done();
         });
     });
   });
@@ -91,19 +87,13 @@ describe('[CATEGORY]'.bold.green, function() {
   describe('#delete()'.cyan, function() {
     it('should delete a specific category', function(done) {
       request(app)
-        .post('/api/categories')
-        .send(category)
+        .delete('/api/categories/' + category._id)
         .set('Accept', 'application/json')
+        .expect('Category-Type', /json/)
+        .expect(200)
         .end(function(err, res) {
-          var category = res.body;
-          request(app)
-            .delete('/api/categories/' + category._id)
-            .expect('Category-Type', /json/)
-            .expect(200)
-            .end(function(err, res) {
-              expect(res.body).to.eql(category);
-              done();
-            });
+          expect(res.body).to.eql(category);
+          done();
         });
     });
   });
